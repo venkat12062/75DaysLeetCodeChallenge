39df879e212be06b88a0c962c4d25583e32cd6ed
venkat12062 <venkat2027c@gmail.com>
2026-03-12 17:24:09 +0530

    Time: 45 ms (27.06%), Space: 47 MB (70.67%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] twoSum(int[] nums, int t) {
+       for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]==t){
+            return new int[]{i,j};
+        }}
+       } 
+       return new int[]{};
+    }
+}
