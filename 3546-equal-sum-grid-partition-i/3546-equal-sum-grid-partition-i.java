b53de6d554e88837b6e261954e3cb99cfd7ec1cf
Venkatramana Tellagorla <venkat2027c@gmail.com>
2026-03-25 07:54:45 +0530

    Time: 4 ms (97.2%), Space: 161.6 MB (44.76%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.java
@@ -2,17 +2,12 @@ class Solution {
     public boolean canPartitionGrid(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
         long total = 0;
-
-        // Step 1: Calculate total sum
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 total += grid[i][j];
             }
         }
-
-        // Step 2: Try horizontal cuts
         long curr = 0;
         for (int r = 0; r < m - 1; r++) {
             for (int j = 0; j < n; j++) {
@@ -23,8 +18,6 @@ class Solution {
                 return true;
             }
         }
-
-        // Step 3: Try vertical cuts
         curr = 0;
         for (int c = 0; c < n - 1; c++) {
             for (int i = 0; i < m; i++) {
