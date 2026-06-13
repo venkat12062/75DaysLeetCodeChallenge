ea44cd4e057b604bcf6b57fe9915aebba46ef040
Venkatramana Tellagorla <venkat2027c@gmail.com>
2026-06-13 22:13:27 +0530

    Time: 2 ms (96.85%), Space: 45.6 MB (99.71%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+      StringBuilder sb=new StringBuilder();
+      for(String word:words){
+        int sum=0;
+        for(char c:word.toCharArray()){
+            sum+=weights[c-'a'];
+        }
+        int value=sum%26;
+        char res=(char)('z'-value);
+        sb.append(res);
+      }  
+      return sb.toString();
+    }
+}
